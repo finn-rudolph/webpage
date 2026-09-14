0c1d125f47e8316e9b854fea8b19f0bc1ba37680
Finn Rudolph <finn.rudolph@t-online.de>
2026-09-13 21:19:45 -0400

    use delay for animation start

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ import { init, frame, setMousePos, hsvToRgb } from "./fluid.js";
 
 const canvas = document.getElementById("fluidCanvas");
 
-let t0 = 600;
+let animation_delay = 300;
+let animation_start = null;
 const animationLength = 800;
 let started = false;
+let event_loop_runs = false;
 const aspect_ratio = canvas.clientWidth / canvas.clientHeight;
 const r = 0.3 * Math.min(1.0, aspect_ratio);
 
 function splash(state, time) {
-  if (!started && time > t0) {
+  if (!event_loop_runs) {
+    event_loop_runs = true;
+    animation_start = time + animation_delay;
+  }
+  if (!started && time > animation_start) {
     started = true;
-    t0 = time;
     state.mouse.isDown = true;
     setMousePos(state, aspect_ratio / 2 + r, 0.5);
     state.mouse.color = hsvToRgb(Math.random(), 0.9, 1.0);
-  } else if (time - t0 > animationLength) {
+  } else if (time - animation_start > animationLength) {
     state.mouse.isDown = false;
   } else {
-    let a = (3 * Math.PI * (time - t0)) / animationLength;
+    let a = (3 * Math.PI * (time - animation_start)) / animationLength;
     setMousePos(
       state,
       0.5 * aspect_ratio + Math.cos(a) * r,
@@ -26,7 +31,7 @@ function splash(state, time) {
     );
   }
 
-  if (time - t0 > 10000) {
+  if (time - animation_start > 10000) {
     state.stop = true;
   }
 }
